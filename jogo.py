1f070bf468ee17adf44fbe8b64821b4063a31576
Gabriel <gabrielfmm@al.insper.edu.br>
2022-11-09 00:12:40 -0300

    Iniciado a criação do jogo

---
+++ jogo.py
@@ -0,0 +1,31 @@
+# imports
+from baseperguntasrespostas import *
+from funcoes import *
+
+# base de perguntas e respostas
+base_processada = transforma_base (quest)
+lista_questoes = []
+
+# id inicial
+id = 1
+
+# lista prêmios
+lista_premios = [0.00, 1000.00, 5000.00, 10000.00, 30000.00, 50000.00, 100000.00, 300000.00, 500000.00, 1000000.00]
+
+
+# tela inicial
+print ('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!''\n')
+nome = input ('Qual seu nome? ')
+print ('\n'f'Ok {nome.upper ()}, você tem direito a pular 3 vezes e 2 ajudas!')
+print ('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
+inicio = input ('Aperte ENTER para continuar...')
+
+# início do jogo
+while True:
+    if inicio == '':
+        print ('\n''O jogo já vai começar! Lá vem a primeira questão!''\n')
+        inicio2 = input ('Vamos começar com questões do nível FACIL!''\n''Aperte ENTER para continuar...')
+        while True:
+            if inicio2 == '':
+                while True and id < 10:
+                    print 
